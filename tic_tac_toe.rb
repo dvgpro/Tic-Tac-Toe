fc9feb31dfc14238e68277a117fb77457ce59e43
dgin <128121988+dvgpro@users.noreply.github.com>
2024-04-17 19:58:52 +0900

    Add function to display the rows of the board

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -2,6 +2,18 @@ class Game
   attr_accessor :board
 
   def initialize
-    @board = Array.new(3) { Array.new(3, empty) }
+    @board = Array.new(3) { Array.new(3) }
+  end
+
+  def display_board
+    row_separators = '-- + -- + --'
+    @board.map { |row| create_rows(row).concat("\n #{row_separators} \n") }
+  end
+
+  def create_rows(row)
+    "  #{row[0]}  |  #{row[1]}  |  #{row[2]}  "
   end
 end
+
+game1 = Game.new
+puts game1.display_board
