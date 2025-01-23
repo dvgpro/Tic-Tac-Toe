28760a98b76974f4c90749d049abb7fea43d3949
dvgpro <dvg.prog@gmail.com>
2025-01-23 22:46:04 +0900

    fix:  Add check to verify user input is between 1 and 9. Add rescue statement when user inputs non-integer.

---
+++ lib/roles/human.rb
@@ -0,0 +1,20 @@
+class Human < Player
+  # Gets and returns user's input
+  def token_location
+    puts "Enter number between 1 and 9"
+
+    begin
+      location = gets.chomps.to_i
+    rescue 
+      return token_location
+    end
+
+    if !(1..9).include?(location)
+      return token_location
+    elsif @game.check_location
+      return token_location
+    end
+
+    return location
+  end
+end
