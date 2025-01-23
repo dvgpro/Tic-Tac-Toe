2907cc324c87dd4a4728719fca377cb4a08c614b
dvgpro <dvg.prog@gmail.com>
2025-01-23 23:30:44 +0900

    feat: Add function to allow player to choose if they want to play against another human or a computer

--- game.rb
+++ game.rb
@@ -5,10 +5,24 @@ class Game
     @game = TicTacToe.new
 
     player_one = Human.new(@game, 'X')
-    player_two = Computer.new(@game, "O")
+    player_two = choose_opponent_control
 
     @game.play_game
   end
+
+  def get_opponent_control
+    puts "Please enter Human or Computer"
+    opponent_class = gets.chomps.capitalize()
+
+    if opponent_class == "Human"
+      return Human.new(@game, "O")
+    elsif opponent_class == "Computer"
+      return Computer.new(@game, "O")
+    end
+
+    return get_opponent_control
+  end  
+
 end
 
 Game.new
